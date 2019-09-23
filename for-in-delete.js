fdc1e1ff046065a76226827b289152b7133ab82c
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-09-22 21:51:21 -0500

    completed JS afternoon 3 exercise

--- for-in-delete.js
+++ for-in-delete.js
@@ -61,6 +61,14 @@ function showValues( obj ) {
 
 //Code Here
 
+function greaterThan10( obj){
+
+  for( var key in obj) {
+    if( obj[key] > 10){
+      obj[key] = 0}
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,7 +80,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for( var key in obj) {
+    obj[key] = obj[key] * 2;
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
--- spread.js
+++ spread.js
@@ -44,7 +44,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
+var result = addNums(...numbers)
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +54,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+let combinedArrays = [...arr1,...arr2];
